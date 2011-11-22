067e8602d6c5a8a7fa634f821ca79d6309170442
itang <live.tang@gmail.com>
2011-11-22 22:49:23 +0800

    cleanup

---
+++ war/public/app/scripts/bookmarkers.js
@@ -0,0 +1,44 @@
+/**
+ * bookmarker.js
+ *
+ */
+(function(){
+$(function() {
+    //refresh click事件 -> iframe reload
+    $('#refresh').click(function() {
+        var iframe = $("#frameWindow");
+        iframe.attr("src", iframe.attr("src"));
+    });
+
+    //点击tab链接，在iframe显示网页
+    $('.sitelink').click(function(event) {
+        var it = this;
+        //刷新
+        $("#frameWindow").attr("src", it.href);
+
+        if(it.href.indexOf("github") > -1){ //github 禁止iframe it
+            return true;
+        }
+
+        //增加点击率
+        (function updateHit() {
+            var name = $(it).attr("data-it");
+            $.ajax({
+                url:"/apps/bookmarkers/" + name + "/inc_hits",
+                type:"POST",
+                dataType:"json",
+                success: function(result) {
+                    if (result.success){
+                        $("#" + name + "hits").html( result.data.currHits );
+                    }else{
+                     alert("update hit error:" + result.message);
+                    }
+                }
+            })
+        })();
+
+        event.preventDefault();
+        event.stopPropagation();
+    });
+});
+})();

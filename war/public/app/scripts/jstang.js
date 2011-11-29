4c81714fb74286c94851aecd8ae29fcc6b3bdffd
itang <live.tang@gmail.com>
2011-11-29 23:03:13 +0800

    some js

---
+++ war/public/app/scripts/jstang.js
@@ -0,0 +1,23 @@
+/**
+ * jstang.js
+ */
+(function(global) {
+  function namespace(names) {
+    if (!names) return global;
+
+    var names_array = names.split(".");
+    var target = global;
+    for (var i = 0; i < names_array.length; i++) {
+      var name = names_array[i];
+      target[name] = target[name] || {};
+      target = target[name];
+    }
+    return target;
+  }
+
+  namespace("jstang");
+  jstang.ns = namespace;
+  return jstang;
+})(window);
+
+

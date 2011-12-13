9988e09546fdb77223bbbd5fc1c57197085d80b4
itang <live.tang@gmail.com>
2011-12-13 20:21:53 +0800

    update midje version

--- project.clj
+++ project.clj
@@ -12,8 +12,8 @@
                  ]
   :dev-dependencies [[appengine-magic "0.4.6"]
                      [lein-eclipse "1.0.0"]
-                     [midje "1.3-alpha5"]
-                     [lein-midje "1.0.4"]]
+                     [midje "1.3.0"]
+                     [lein-midje "1.0.7"]]
   :aot [www-itang-me.app_servlet])
 
 

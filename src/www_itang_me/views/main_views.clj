09f0a5d2f39fe0640554c932e832c8d52d46d438
itang <live.tang@gmail.com>
2012-03-15 23:39:56 +0800

    bootstrap2.x

--- src/www_itang_me/views/main_views.clj
+++ src/www_itang_me/views/main_views.clj
@@ -38,28 +38,28 @@
            [:p (now)]
            [:a {:href "#" :class "btn pull-center"} "更多"]]]]]
 
+       (include-app-js "github-client")
+       ;;(include-app-roy "github-client")
+       ;;(include-app-js "roy-loader")
+
        [:section#github_section {}
         [:div.page-header {}
          [:h1 "项目" "&nbsp;" [:small "参与或关注的项目"]]]
         [:div.row-fluid {}
          ;;github widget
-         (include-app-js "github-client")
-         ;;(include-app-roy "github-client")
-         ;;(include-app-js "roy-loader")
          [:div.span5 {}
           [:h3 "关注的项目"]
-          [:div {}
-           [:ul#mywatched {}
-            (javascript-tag-ext {:id "repos-template" :type "text/x-handlebars-template"}
-              [:li {}
-               "{{index}}&nbsp;"
-               [:a {:href "{{html_url}}{{website}}" :title "{{description}}\n{{updated_at}}" :target "_blank"}
-                "{{name}}"]
-               [:small "({{language}})"]])]
-           [:a {:href "/projects" :class "btn pull-center"} "更多"]]]
+          [:ul#mywatched {}
+           (javascript-tag-ext {:id "repos-template" :type "text/x-handlebars-template"}
+             [:li {}
+              "{{index}}&nbsp;"
+              [:a {:href "{{html_url}}{{website}}" :title "{{description}}\n{{updated_at}}" :target "_blank"}
+               "{{name}}"]
+              [:small "({{language}})"]])]
+          [:a {:href "/projects" :class "btn pull-center"} "更多"]]
          [:div.span4 {}
           [:h3 "我的项目"]
-          [:div [:ul#myrepos ]]]]] ;;end section@github
+          [:ul#myrepos ]]]] ;;end section@github
        (get-footer app)])))
 
 
